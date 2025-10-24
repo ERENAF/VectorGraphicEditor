d86acc60366892aaa598b4ad76015f7f2ce26030
Fanera <egor.powwer@gmail.com>
2025-10-24 11:08:53 +1000

    shape+rectangle

---
+++ shapes/Rectangle.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorEditor.shapes;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace VectorEditor.shapes
+{
+    public class Rectangle: Shape
+    {
+        public override void Draw (Graphics g)
+        {
+            DrawWithTransformations(g, (g) =>
+            {
+                using (var fillBrush = new SolidBrush(Color.FromArgb((int)(Opacity * 255), FillColor)))
+                using (var strokePen = new Pen(StrokeColor, StrokeWidth))
+                {
+                    g.FillRectangle(fillBrush, Position.X, Position.Y, Size.width, Size.height);
+                    g.DrawRectangle(strokePen, Position.X, Position.Y, Size.width, Size.height);
+                    if (IsSelected)
+                    {
+                        DrawSelection(g);
+                    }
+                }
+            }
+            );
+        }
+
+        private void DrawSelection(Graphics g)
+        {
+            using (var selectedPen = new Pen(Color.Red, StrokeWidth)
+            {
+                DashPattern = new float[] { 3, 3 },
+                DashStyle = DashStyle.Custom
+            })
+            {
+                g.DrawRectangle(selectedPen, Position.X, Position.Y, Size.width, Size.height);
+            }
+            DrawTranformHandles(g);
+        }
+
+        private void DrawTranformHandles(Graphics g)
+        {
+            var bounds = GetBounds();
+            var center = GetRotationCenter();
+            
+            var rotationHandle = new PointF(center.X, bounds.Y - 20);
+            using (var handleBrush = new SolidBrush(Color.Blue))
+            using (var handlePen = new Pen(Color.Blue,1))
+            {
+                g.DrawLine(handlePen, center, rotationHandle);
+                g.FillEllipse(handleBrush, rotationHandle.X - 4, rotationHandle.Y - 4, 8, 8);
+                g.FillEllipse(handleBrush, rotationHandle.X - 4, rotationHandle.Y - 4, 8, 8);
+                DrawScaleHandles(g, bounds);
+            }
+        }
+        private void DrawScaleHandles(Graphics g, RectangleF bounds)
+        {
+            var corners = new[]
+            {
+                new PointF(bounds.Left, bounds.Top),
+                new PointF(bounds.Right, bounds.Top),
+                new PointF(bounds.Right, bounds.Bottom),
+                new PointF(bounds.Left, bounds.Bottom)
+            };
+            using (var handleBrush = new SolidBrush(Color.Green))
+            {
+                foreach (var corner in corners)
+                {
+                    g.FillRectangle(handleBrush, corner.X - 3, corner.Y - 3, 6, 6);
+                }
+            }
+        }
+
+        public override bool ContainsPoint(PointF point)
+        {
+            var localPoint = TransformPoint(point, true);
+            return localPoint.X >= Position.X && localPoint.X <= Position.X + Size.width &&
+                localPoint.Y >= Position.Y && localPoint.Y <= Position.Y + Size.height;
+        }
+
+        public override RectangleF GetBounds()
+        {
+            if (Rotation == 0 && Scale.X == 0 && Scale.Y == 0)
+            {
+                return new RectangleF(Position.X,Position.Y, Size.width, Size.height);
+            }
+
+            var corners = new PointF[]
+            {
+                new PointF(Position.X, Position.Y),
+                new PointF(Position.X+Size.width,Position.Y),
+                new PointF(Position.X+Size.width, Position.Y+Size.height),
+                new PointF(Position.X,Position.Y+Size.height)
+            };
+
+            using (var matrix = GetTransfromationMatrix())
+            {
+                matrix.TransformPoints(corners);
+            }
+            float minX = corners.Min(p => p.X);
+            float minY = corners.Min(p => p.Y);
+            float maxX = corners.Max(p => p.X);
+            float maxY = corners.Max(p => p.Y);
+
+            return new RectangleF(minX,minY, maxX-minX, maxY-minY);
+        }
+
+        public bool IsScaleHandleClicked(PointF point)
+        {
+            if (!IsSelected) return false;
+
+            var bounds = GetBounds();
+            var corners = new[]
+            {
+                new PointF(bounds.Left, bounds.Top),
+                new PointF(bounds.Right, bounds.Top),
+                new PointF(bounds.Right, bounds.Bottom),
+                new PointF(bounds.Left, bounds.Bottom)
+            };
+            foreach (var cor in corners)
+            {
+                var transformedCorner = TransformPoint(cor);
+                var handleRect = new RectangleF(transformedCorner.X - 6, transformedCorner.Y - 6, 12, 12);
+                if (handleRect.Contains(point)) { return true; }
+            }
+            return false;
+        }
+    }
+}
---
+++ shapes/Shape.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
+
+namespace VectorEditor.shapes
+{
+    public struct Scale
+    {
+        public float X;
+        public float Y;
+    };
+    public struct Size
+    {
+        public float width;
+        public float height;
+    }
+    public abstract class Shape
+    {
+        public Guid Id;
+        private PointF _position;
+        private float _rotation;
+        private Size _size;
+        private Scale _scale;
+        private Color _fillColor = Color.White;
+        private Color _strokeColor = Color.Black;
+        private float _strokeWidth = 1.0f;
+        private float _opacity = 1.0f;
+        private bool _isSelected;
+        
+        public PointF Position
+        {
+            get => _position; set => _position = value;
+        }
+        public float Rotation
+        {
+            get => _rotation; set => _rotation = value;
+        }
+        public Size Size
+        {
+            get => _size;
+            set
+            {
+                if (value.width < 0 || value.height < 0)
+                {
+                    throw new ArgumentException("Value can't be negative");
+                }
+                _size = value;
+            }
+        }
+        public Scale Scale
+        {
+            get => _scale; 
+            set
+            {
+
+                if (value.X < 0 || value.Y < 0)
+                {
+                    throw new ArgumentException("Scale can't be negative");
+                }
+                _scale = value;
+            }
+        }
+        public Color FillColor
+        {
+            get => _fillColor; set => _fillColor = value;
+        }
+        public Color StrokeColor
+        {
+            get => _strokeColor; set => _strokeColor = value;
+        }
+        public float StrokeWidth
+        {
+            get => _strokeWidth;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Stroke width can't be negative");
+                }
+                _strokeWidth = value;
+            }
+        }
+        public float Opacity
+        {
+            get => _opacity;
+            set
+            {
+                if (value < 0  || value > 100)
+                {
+                    throw new ArgumentException("value should be from 0 to 100");
+                }
+                _opacity = value;
+            }
+        }
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => _isSelected = value;
+        }
+        public Color ApplyOpacity()
+        {
+            return Color.FromArgb((int)(Opacity * 255),FillColor);
+        }
+        public virtual PointF GetRotationCenter()
+        {
+            var bounds = GetBounds();
+            return new PointF(bounds.X + bounds.Width / 2, bounds.Y + bounds.Height / 2);
+        }
+        protected virtual void ApplyTransformations(Graphics graphics)
+        {
+            if (Rotation == 0 && Scale.X == 1 && Scale.Y == 1)
+            {
+                return;
+            }
+            var center = GetRotationCenter();
+
+            graphics.TranslateTransform(center.X, center.Y);
+            graphics.RotateTransform(Rotation);
+            graphics.ScaleTransform(Scale.X, Scale.Y);
+            graphics.TranslateTransform(-center.X, -center.Y);
+        }
+        protected virtual Matrix GetTransfromationMatrix()
+        {
+            var matrix = new Matrix();
+            var center = GetRotationCenter();
+
+            matrix.Translate(center.X, center.Y);
+            matrix.Rotate(Rotation);
+            matrix.Scale(Scale.X, Scale.Y);
+            matrix.Translate(-center.X, -center.Y);
+            return matrix;
+        }
+        protected virtual PointF TransformPoint(PointF point, bool inverse = false)
+        {
+            using (var matrix = GetTransfromationMatrix())
+            {
+                if (inverse)
+                {
+                    var inverseMatrix = matrix.Clone();
+                    inverseMatrix.Invert();
+                    var points = new[] { point };
+                    inverseMatrix.TransformPoints(points);
+                    return points[0];
+                }
+                else
+                {
+                    var points = new[] { point };
+                    matrix.TransformPoints(points);
+                    return points[0];
+                }
+            }
+        }
+
+        protected void DrawWithTransformations(Graphics g, Action<Graphics> drawAction)
+        {
+            var saveState = g.Save();
+            try
+            {
+                ApplyTransformations(g);
+                drawAction(g);
+            }
+            finally
+            {
+                g.Restore(saveState);
+            }
+        }
+        public abstract void Draw(Graphics g);
+        public abstract bool ContainsPoint(PointF point);
+        public abstract RectangleF GetBounds();
+    }
+
+}
